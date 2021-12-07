af8910c9a449593743c7a886fa485f56f7b8b0fd
Nick <41147237+NickRydensky@users.noreply.github.com>
2021-12-07 22:24:12 +0200

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,1 +1,11 @@
-alert('hello world')
+let hi = confirm('будем здороваться?')
+if(hi === true){
+    alert('привет!')
+    let name = prompt('как тебя зовут?')
+    if(name.length>0){
+        alert('привет ,' + name)
+    }
+}
+else{
+    alert('как хочешь')
+}
