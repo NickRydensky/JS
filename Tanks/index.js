f1c6a91f38a9c772e0035539e82087f475f95aa9
Denis Simancuk <sidenser@gmail.com>
2022-01-19 19:52:38 +0200

    заготовка

--- Tanks/index.js
+++ Tanks/index.js
@@ -3,11 +3,23 @@
 let canv = document.getElementById('field');
 ctx = canv.getContext('2d');
 
-ctx.fillStyle = 'blue'
-ctx.fillRect(0, 0, 50, 50);
-ctx.fillRect(0, 50, 50, 50);
-ctx.fillRect(100, 0, 50, 50);
-ctx.fillRect(200, 0, 50, 50);
-ctx.fillRect(100, 50, 50, 50);
-ctx.fillRect(50, 150, 50, 50);
-ctx.fillRect(150, 150, 50, 50);
+console.log()
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function cub(x, y, color = 'blue')
+{
+    ctx.clearRect(0,0,canv.width,canv.height); //стираем нарисованое до этого
+    ctx.fillStyle = color //задаем цвет
+    ctx.fillRect(x, y, 50, 50); //  квадрат
+}
+
+let colors = ['red', 'blue', 'green'];
+
+function fps(){
+    let color = colors[randomIntFromInterval(0, 3)]
+    cub(randomIntFromInterval(0, canv.width - 50), randomIntFromInterval(0, canv.height - 50), color)
+}
+
+setInterval(fps, 1000/30);
