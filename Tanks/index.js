4fcdd810b33dcd54363c235049df1cac2c51b70d
Nick <41147237+NickRydensky@users.noreply.github.com>
2022-01-19 19:30:22 +0200

    Add files via upload

---
+++ Tanks/index.js
@@ -0,0 +1,13 @@
+// function draw(){
+
+let canv = document.getElementById('field');
+ctx = canv.getContext('2d');
+
+ctx.fillStyle = 'blue'
+ctx.fillRect(0, 0, 50, 50);
+ctx.fillRect(0, 50, 50, 50);
+ctx.fillRect(100, 0, 50, 50);
+ctx.fillRect(200, 0, 50, 50);
+ctx.fillRect(100, 50, 50, 50);
+ctx.fillRect(50, 150, 50, 50);
+ctx.fillRect(150, 150, 50, 50);
